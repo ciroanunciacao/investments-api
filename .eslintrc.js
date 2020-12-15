be29c18b20cb46b2aee6195404043b180b0c6cc0
Ciro Anunciação <ciro.anunciacao@gmail.com>
2020-12-15 14:40:44 -0300

    feat: add project setup

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        semi: 'off',
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+        'no-underscore-dangle': ['error', { allow: ['__resolveReference'] }],
+        'import/no-extraneous-dependencies': ['error', { optionalDependencies: true }],
+      },
+    },
+  ],
+};
