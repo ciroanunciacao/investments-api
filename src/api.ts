12264bfe04402fbafed97765620e6953725a1115
Ciro Anunciação <ciro.anunciacao@gmail.com>
2020-12-15 14:45:26 -0300

    feat: add api and local server class

---
+++ src/api.ts
@@ -0,0 +1,69 @@
+import 'reflect-metadata';
+import {
+  APIGatewayProxyHandler, APIGatewayProxyEvent, Context, APIGatewayProxyResult, Callback,
+} from 'aws-lambda';
+
+import { ApolloServer } from 'apollo-server-lambda';
+import { buildSchema } from 'type-graphql';
+import resolvers from './delivery/graphql/resolvers';
+import Logger from './infrastructure/utils/Logger';
+import { consts } from './infrastructure/config';
+import initialize from './infrastructure';
+import { createContext, AppContext } from './delivery/graphql/context';
+
+let handlerGraph: any | undefined;
+
+export const createHandler = async (): Promise<any> => {
+  const schema = await buildSchema({ resolvers, dateScalarMode: 'isoDate' });
+  return new ApolloServer({
+    schema,
+    introspection: true,
+    context: async ({ context, event }) => {
+      const data: AppContext = await createContext(context, event);
+      return data;
+    },
+    playground: {
+      endpoint: '/dev/graphql',
+    },
+  }).createHandler();
+};
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent, context: Context, callback: Callback<APIGatewayProxyResult>,
+): Promise<any> => {
+  const handlerCallback: Callback<APIGatewayProxyResult> = (
+    error?: Error | string | null,
+    result?: APIGatewayProxyResult,
+  ) => {
+    if (error) {
+      Logger.e(consts.APP.TAG, 'Error on handler callback', error);
+    }
+    callback(error, result);
+  };
+
+  if (typeof handlerGraph === 'function') {
+    Logger.i(consts.APP.TAG, 'Reusing connection');
+    handlerGraph(event, context, handlerCallback);
+  } else {
+    Logger.i(consts.APP.TAG, 'Creating database connection');
+
+    try {
+      handlerGraph = await createHandler();
+      await initialize();
+      Logger.i(consts.APP.TAG, 'Database connected');
+      handlerGraph(event, context, handlerCallback);
+    } catch (error) {
+      Logger.e(consts.APP.TAG, 'Error creating database connection', error);
+      handlerGraph = undefined;
+      const result: APIGatewayProxyResult = {
+        statusCode: 500,
+        body: JSON.stringify(error, Object.getOwnPropertyNames(error)),
+        isBase64Encoded: false,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      handlerCallback(null, result);
+    }
+  }
+};
---
+++ src/server.ts
@@ -0,0 +1,140 @@
+import * as http from 'http';
+import * as url from 'url';
+import {
+  APIGatewayProxyEvent, APIGatewayProxyResult, Context, Callback,
+} from 'aws-lambda';
+
+import { handler } from './api';
+import Logger from './infrastructure/utils/Logger';
+
+const TAG = 'server';
+
+const handleServer: http.RequestListener = (
+  req: http.IncomingMessage, res: http.ServerResponse,
+): void => {
+  const { headers, method } = req;
+
+  const multiValueHeaders: { [name: string]: string[] } = { 'x-empty': [''] };
+
+  const event: APIGatewayProxyEvent = {
+    httpMethod: method,
+    body: '',
+    headers: headers as { [name: string]: string },
+    multiValueHeaders,
+    isBase64Encoded: false,
+    queryStringParameters: url.parse(req.url, true).query as { [name: string]: string },
+    multiValueQueryStringParameters: null,
+    pathParameters: null,
+    path: req.url,
+    stageVariables: {},
+    resource: '',
+    requestContext: {
+      authorizer: null,
+      protocol: 'https',
+      accountId: new Date().getTime().toString(16),
+      apiId: new Date().getTime().toString(16),
+      stage: new Date().getTime().toString(16),
+      httpMethod: method,
+      identity: {
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        principalOrgId: null,
+        sourceIp: '',
+        user: null,
+        userAgent: null,
+        userArn: null,
+      },
+      path: req.url,
+      requestId: new Date().getTime().toString(16),
+      resourceId: new Date().getTime().toString(16),
+      resourcePath: req.url,
+      requestTimeEpoch: new Date().getTime(),
+      requestTime: new Date().toJSON(),
+    },
+  };
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, Cache-Control, postman-token',
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  const buffer: any[] = [];
+
+  const promisor = {
+    handleRequestData: (chunk: any) => {
+      buffer.push(chunk);
+    },
+    handleRequestFinish: async () => {
+      const body = Buffer.concat(buffer).toString();
+      event.body = body;
+
+      const context: Context = {
+        functionVersion: '',
+        awsRequestId: new Date().getTime().toString(16),
+        functionName: '',
+        invokedFunctionArn: '',
+        callbackWaitsForEmptyEventLoop: true,
+        memoryLimitInMB: '256',
+        logGroupName: '',
+        logStreamName: '',
+        getRemainingTimeInMillis: () => 200,
+        done: (error?: Error, result?: any) => {
+          Logger.d(TAG, 'Called done', { error, result });
+        },
+        fail: (error: Error | string) => {
+          Logger.d(TAG, 'Called fail', { error });
+        },
+        succeed: (messageOrObject: any) => {
+          Logger.d(TAG, 'Called succeed', { messageOrObject });
+        },
+      };
+
+      const callback: Callback<APIGatewayProxyResult> = (
+        error?: Error | string | null,
+        result?: APIGatewayProxyResult,
+      ) => {
+        if (result) {
+          const responseHeaders = result.headers || {};
+          Object.keys(responseHeaders).forEach((header) => {
+            res.setHeader(header, responseHeaders[header].toString());
+          });
+          res.writeHead(result.statusCode);
+          res.write(result.body);
+          res.end();
+        } else {
+          res.writeHead(500);
+          res.write(JSON.stringify(error));
+          res.end();
+        }
+      };
+
+      handler(event, context, callback);
+    },
+  };
+
+  req
+    .on('data', promisor.handleRequestData)
+    .on('end', promisor.handleRequestFinish);
+};
+
+const listenServer = () => {
+  Logger.i(TAG, 'Server running at http://localhost:3000');
+};
+
+http.createServer(handleServer).listen(3000, listenServer);
